7f505aed91f3234bdcf93209365aafd166da07b4
Akhil Raghav <86706467+AKhilRaghav0@users.noreply.github.com>
2023-12-16 11:40:52 +0530

    Initial Commit

---
+++ networkcalls/networkcallsApp.swift
@@ -0,0 +1,17 @@
+//
+//  networkcallsApp.swift
+//  networkcalls
+//
+//  Created by Akhil on 16/12/23.
+//
+
+import SwiftUI
+
+@main
+struct networkcallsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
